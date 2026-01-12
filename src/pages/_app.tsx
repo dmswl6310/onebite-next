7e01272af06327e0f79e1400fe9efa3c46c4da05
dmswl6310 <dmswl6310@gmail.com>
2026-01-12 15:25:31 +0900

    fix: 화면 미표시 현상 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,9 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  <GlobalLayout>
-    <Component {...pageProps} />
-  </GlobalLayout>;
+  return (
+    <GlobalLayout>
+      <Component {...pageProps} />;
+    </GlobalLayout>
+  );
 }
