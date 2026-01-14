d17af7eee4fc3209a14436db83308aacc2d39268
dmswl6310 <dmswl6310@gmail.com>
2026-01-14 16:45:31 +0900

    feat: 검색 기능 추가
    1. 새로고침시에도 검색어 유지
    2. 검색어와 qeurystring이 이미 같으면 이동X
    3. input박스에서 엔터시 바로 submit되게

--- src/components/searchable-layout.tsx
+++ src/components/searchable-layout.tsx
@@ -1,13 +1,48 @@
-import { ReactNode } from "react";
+import { useRouter } from "next/router";
+import { ReactNode, useEffect, useState } from "react";
 
 export default function SearchableLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+  const q = router.query.q as string;
+
+  // 새로고침시에도 검색어 유지
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    // 검색어와 querystring이 이미 같으면 이동X
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  // 엔터로 바로 submit 되게
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
     <div>
-      <div>임시 서치바</div>
+      <div>
+        <input
+          value={search}
+          onKeyDown={onKeyDown}
+          onChange={onChangeSearch}
+          placeholder="검색어를 입력하세요..."
+        />
+        <button onClick={onSubmit}>검색</button>
+      </div>
       {children}
     </div>
   );
