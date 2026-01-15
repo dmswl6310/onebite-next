51bb2f7db8c92e25325f05256a80f97419dca29c
dmswl6310 <dmswl6310@gmail.com>
2026-01-15 15:45:01 +0900

    feat: 정적경로에 SSG적용하기

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,10 +1,8 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(Number(id));
 
@@ -13,9 +11,20 @@ export const getServerSideProps = async (
   };
 };
 
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: false,
+  };
+};
+
 export default function Page({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   if (!book) return "문제가 발생했습니다 다시 시도하세요";
 
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
