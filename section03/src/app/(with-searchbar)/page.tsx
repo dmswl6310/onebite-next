51cea8f56938069feaa53d96877b2bbad273185d
dmswl6310 <dmswl6310@gmail.com>
2026-01-27 11:59:59 +0900

    feat: search페이지와 메인페이지 suspense 적용

--- section03/src/app/(with-searchbar)/page.tsx
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,10 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
-export const dynamic = "auto";
+// export const dynamic = "auto";
 // 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
 // 1. auto : 기본값, 아무것도 강제하지 않음
 // 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
@@ -10,6 +12,7 @@ export const dynamic = "auto";
 // 4. error: 페이지를 강제로 Static 페이지 설정 (설정하면 안되는 이유가 있으면, 빌드 오류 발생시킴)
 
 async function AllBooks() {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" },
@@ -19,7 +22,6 @@ async function AllBooks() {
   }
 
   const allBooks: BookData[] = await response.json();
-  console.log(allBooks);
 
   return (
     <div>
@@ -30,7 +32,10 @@ async function AllBooks() {
   );
 }
 
+export const dynamic = "force-dynamic";
+
 async function RecoBooks() {
+  await delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } },
@@ -49,16 +54,20 @@ async function RecoBooks() {
   );
 }
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
