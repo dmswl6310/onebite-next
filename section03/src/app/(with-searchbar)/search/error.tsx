f28d0bca50739ce04c7acdaec3b28272caef6a9a
dmswl6310 <dmswl6310@gmail.com>
2026-02-02 19:10:49 +0900

    fix: 주석 수정

--- section03/src/app/(with-searchbar)/search/error.tsx
+++ section03/src/app/(with-searchbar)/search/error.tsx
@@ -21,7 +21,7 @@ export default function Error({
       <button
         onClick={() => {
           startTransition(() => {
-            // 안에 실행값ㅇ르 일괄적으로 UI 업데이트
+            // 안에 실행값을 일괄적으로 UI 업데이트
             router.refresh(); // 현재 페이지에 필요한 서버 컴포넌트들을 다시 불러옴
             reset(); // 에러 상태를 초기화, 컴포넌트들을 다시 렌더링
           });
